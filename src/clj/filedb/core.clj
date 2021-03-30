53ca9d3ed8299ac3ff59dd12d2d820963362ad22
Alan Thompson <cloojure@gmail.com>
2021-03-30 15:25:26 -0700

    misc

--- src/clj/filedb/core.clj
+++ src/clj/filedb/core.clj
@@ -1,64 +1,101 @@
 (ns filedb.core
-  ; (:gen-class) ; uncomment to make runnable JAR file
+  (:refer-clojure :exclude [load save ])
+  (:use tupelo.core)
   (:require
     [clojure.edn :as edn]
     [schema.core :as s]
     [tupelo.io :as tio]
     [tupelo.misc :as misc]
-    )
+    [tupelo.schema :as tsk])
   (:import
     [java.io File]
     [java.nio.file Path]
     [java.time Instant]
     ))
 
+(s/def ^:dynamic *filedb-root-dir* :- s/Str
+   "./filedb.d")
+
 (s/defn java-time-instant-now :- Instant
   "Overridable version of java.time.Instant/now (for testability)"
   [] (Instant/now))
 
-(s/defn write-file
-  "Saves an EDN data structure to a HashFile"
-  [file-spec :- (s/cond-pre s/Str File Path)
-   data-value :- s/Any]
+(s/defn ^:no-doc build-hashfile-map :- tsk/KeyMap
+  "Build a HashFile data map"
+  [data-value :- s/Any]
   (let [data--string (pr-str (misc/normalized-sorted data-value))
         data--hash   (misc/str->sha data--string)
         hashfile-map {:file/type    :hash/file
                       :file/format  :v20.03.28
-                      :time/instant (Instant/now)
+                      :time/instant (str (java-time-instant-now))
                       :hash/type    :hash/sha-1
                       :data/hash    data--hash
                       :data/type    :data/edn
-                      :data/string  data--string}
-        hashfile-str (pr-str hashfile-map)
-        ]
-    (spit (tio/->File file-spec) hashfile-str)))
+                      :data/string  data--string}]
+    hashfile-map))
 
-(s/defn load-file
+(s/defn ^:no-doc parse-hashfile-map :- s/Any
   "Loads and parses a HashFile"
-  [file-spec :- (s/cond-pre s/Str File Path)]
-  (let [hashfile-str  (slurp (tio/->File file-spec))
-        hashfile-map  (edn/read-string hashfile-str)
-
-        file--type    (fetch :file/type hashfile-map)
-        file--format  (fetch :file/format hashfile-map)
-        time--instant (Instant/parse (fetch :time/instant hashfile-map))
-        hash--type    (fetch :hash/type hashfile-map)
-        data--type    (fetch :data/type hashfile-map)
-        data--hash    (fetch :data/hash hashfile-map) ; a String like "a9993e364706816aba3e25717850c26c9cd0d89d"
-        data--string  (fetch :data/string hashfile-map) ; a String like "{:a 1 :b [2 3] }"
+  [hashfile-map :- tsk/KeyMap]
+  (let [file--type    (grab :file/type hashfile-map)
+        file--format  (grab :file/format hashfile-map)
+        time--instant (Instant/parse (grab :time/instant hashfile-map)) ; verify legal instant (can parse)
+        hash--type    (grab :hash/type hashfile-map)
+        data--type    (grab :data/type hashfile-map)
+        data--hash    (grab :data/hash hashfile-map) ; a String like "a9993e364706816aba3e25717850c26c9cd0d89d"
+        data--string  (grab :data/string hashfile-map) ; a String like "{:a 1 :b [2 3] }"
 
         >>            (assert (= file--type :hash/file))
         >>            (assert (= file--format :v20.03.28))
+        >>            (assert (instance? Instant time--instant))
         >>            (assert (= hash--type :hash/sha-1))
         >>            (assert (= data--type :data/edn))
         >>            (assert (= data--hash (misc/str->sha data--string)))
 
-        data-value    (edn/read-string data--string)
-        ]
-    ))
+        data-value    (edn/read-string data--string)]
+    data-value))
 
-(defn -main []
-  (println "main - enter")
-  )
+(s/defn file-key->file-abs :- File
+  [file-key :- s/Str]
+  (let [filespec-rel-str (str (tio/->File (str file-key ".edn")))
+        filespec-abs     (File. (tio/->File *filedb-root-dir*)
+                           filespec-rel-str)]
+    (spyx filespec-abs)
+    filespec-abs))
+
+(s/defn save
+  "Saves an EDN data structure to a HashFile under the directory `*filedb-root-dir*`.
+
+   The arg `file-key` is a unique String identifier which must be a legal relative directory path like:
+
+         joe
+         cust/joe
+         cust/2019/joe
+
+   where use of a file separator like `/` on Unix/OSX will result in nested directories. "
+  [file-key :- s/Str
+   data-value :- s/Any]
+  (let [filespec-abs (file-key->file-abs file-key)
+        hashfile-str (pr-str (build-hashfile-map data-value))]
+    (tio/mkdirs-parent filespec-abs)
+    (spit filespec-abs hashfile-str)))
+
+(s/defn load :- s/Any
+  "Loads and parses an EDN data structure from a HashFile under the directory `*filedb-root-dir*`.
+
+   The arg `file-key` is a unique String identifier which must be a legal relative directory path like:
+
+         joe
+         cust/joe
+         cust/2019/joe
+
+   where use of a file separator like `/` on Unix/OSX will result in nested directories. "
+  [file-key :- s/Str ]
+  (let [filespec-abs (file-key->file-abs file-key)
+        hashfile-str (slurp filespec-abs)
+        hashfile-map (edn/read-string hashfile-str)
+        data-value   (parse-hashfile-map hashfile-map)]
+    data-value))
+
 
 
--- test/clj/tst/filedb/core.clj
+++ test/clj/tst/filedb/core.clj
@@ -1,10 +1,12 @@
 (ns tst.filedb.core
+  (:refer-clojure :exclude [load save ])
   (:use filedb.core tupelo.core tupelo.test)
   (:require
-    [clojure.edn :as edn]
     [schema.core :as s]
     [tupelo.misc :as misc]
-    [tupelo.string :as str])
+    [tupelo.string :as str]
+    [tupelo.io :as tio]
+    [tupelo.schema :as tsk])
   (:import
     [java.time Instant]
     ))
@@ -19,15 +21,72 @@
    :data/string  "{:a 1 :b [2 3] }" ; an EDN string as from `pr` (notice the double-quotes). NOT an EDN data structure
    })
 
+(def instant-party (Instant/parse "1999-12-31t23:44:55.666Z"))
+
 (dotest
-  (let [instant-party (Instant/parse "1999-12-31t23:59:59.123Z")
-        ]
-    (spyx instant-party)
-    )
+  (let [abc3          "abc"
+        abc5          (pr-str abc3)] ; adds double-quotes => 5 chars total
+    (is= 3 (count abc3))
+    (is= 5 (count abc5))
+    (is= (misc/str->sha abc3) "a9993e364706816aba3e25717850c26c9cd0d89d")
+    (is= (misc/str->sha abc5) "b87f4bf9b7b07f594430548b653b4998e4b40402")))
 
+(dotest
+  (with-redefs [java-time-instant-now (const-fn instant-party)]
+    (is= instant-party (java-time-instant-now))
+    (let [result (it-> (build-hashfile-map "abc")
+                   (update-in it [:data/string] str/quotes->single)
+                   (update-in it [:time/instant] str))]
+      (is= result {:file/type    :hash/file,
+                   :file/format  :v20.03.28,
+                   :time/instant "1999-12-31T23:44:55.666Z",
+                   :hash/type    :hash/sha-1,
+                   :data/hash    "b87f4bf9b7b07f594430548b653b4998e4b40402",
+                   :data/type    :data/edn,
+                   :data/string  "'abc'"}))))
 
+(defn verify-hashfile-map-round-trip
+  [arg]
+  (let [hf-map (build-hashfile-map arg)
+        >>     (s/validate tsk/KeyMap hf-map)
+        result (parse-hashfile-map hf-map)]
+    (is= result arg)))
 
-  )
+(dotest
+  (verify-hashfile-map-round-trip 5)
+  (verify-hashfile-map-round-trip "abc")
+  (verify-hashfile-map-round-trip [1 2 3])
+  (verify-hashfile-map-round-trip {:a 1 :b [2 3 4]})
+  (verify-hashfile-map-round-trip {:a 1 :b [9 "hello"]})
+  (verify-hashfile-map-round-trip {:a 1 :b [9 "hello" #{9 8 7 2}]}))
+
+(defn verify-hashfile-round-trip
+  [arg]
+  (let [fname (format "some/tree/dir/struct/tmp--%d" (System/nanoTime))]
+    (save fname arg)
+    (let [result (load fname)]
+      (is= result arg))))
+
+(dotest
+  (let [tmpdir (tio/create-temp-directory "filedb")]
+    (binding [*filedb-root-dir* tmpdir]
+      (verify-hashfile-round-trip 5)
+      (verify-hashfile-round-trip "abc")
+      (verify-hashfile-round-trip [1 2 3])
+      (verify-hashfile-round-trip {:a 1 :b [2 3 4]})
+      (verify-hashfile-round-trip {:a 1 :b [9 "hello"]})
+      (verify-hashfile-round-trip {:a 1 :b [9 "hello" #{9 8 7 2}]}))))
+
+(dotest
+  ; (binding [*filedb-root-dir* (tio/->File "./filedb-tmp-1519")])
+
+  (verify-hashfile-round-trip 5)
+  (verify-hashfile-round-trip "abc")
+  (verify-hashfile-round-trip [1 2 3])
+  (verify-hashfile-round-trip {:a 1 :b [2 3 4]})
+  (verify-hashfile-round-trip {:a 1 :b [9 "hello"]})
+  (verify-hashfile-round-trip {:a 1 :b [9 "hello" #{9 8 7 2}]}))
+
 
 
 
